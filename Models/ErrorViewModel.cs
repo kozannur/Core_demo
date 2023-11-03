71d9910e51f4ae1c2eaf5d1bd4294c2d55b49dbb
kozan <kozan@YAYGUMUSNUR>
2023-11-03 15:31:09 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Core_Proje_1251.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
